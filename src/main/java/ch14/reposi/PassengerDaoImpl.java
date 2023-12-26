8475486fd2a7b19b95a44c2c2b812a61e4983168
osparking <jbpark03@gmail.com>
2023-12-26 13:23:49 +0900

    passenger 관리 4 메소드 구현

---
+++ src/main/java/ch14/reposi/PassengerDaoImpl.java
@@ -0,0 +1,70 @@
+package ch14.reposi;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ch14.passen.Passenger;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PassengerDaoImpl implements PassengerDao {
+
+  private Connection connection;
+
+  @Override
+  public void insert(Passenger passenger) {
+    String sql = "INSERT INTO PASSENGERS (ID, NAME) VALUES (?, ?)";
+
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, passenger.getIdentifier());
+      statement.setString(2, passenger.getName());
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void update(String id, String name) {
+    String sql = "UPDATE PASSENGERS SET NAME = ? WHERE ID = ?";
+
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, name);
+      statement.setString(2, id);
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void delete(Passenger passenger) {
+    String sql = "DELETE FROM PASSENGERS WHERE ID = ?";
+
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, passenger.getIdentifier());
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Passenger getById(String id) {
+    String sql = "SELECT * FROM PASSENGERS WHERE ID = ?";
+    Passenger passenger = null;
+
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, id);
+      ResultSet rs = statement.executeQuery();
+      if (rs.next()) {
+        passenger = new Passenger(rs.getString(1), rs.getString(2));
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return passenger;
+  }
+}
