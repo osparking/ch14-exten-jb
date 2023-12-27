27bc61e4957a30ffdffd7d27999f4d61a013dfb7
osparking <jbpark03@gmail.com>
2023-12-27 10:47:51 +0900

    승객 추가 메소드 시험 메소드 생성

--- src/test/java/ch14/passen/PassengerTest.java
+++ src/test/java/ch14/passen/PassengerTest.java
@@ -1,19 +1,35 @@
 package ch14.passen;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import ch14.passen.extension.ContextExecutionCondition;
+import ch14.passen.extension.DataOperationExtension;
+import ch14.reposi.PassengerDao;
+import lombok.AllArgsConstructor;
 
-@ExtendWith(ContextExecutionCondition.class)
+@ExtendWith({ ContextExecutionCondition.class, DataOperationExtension.class })
+@AllArgsConstructor
 class PassengerTest {
+
+  private PassengerDao passengerDao;
 
   @Test
   void testPassengerString() {
     var passenger = new Passenger("KR345", "이길동");
     var passengerLee = "승객 [아이디=KR345, 성명=이길동]";
     assertEquals(passengerLee, passenger.toString());
+  }
+
+  @Test
+  void testPassengerInsert() {
+    String custID = "123-345-345";
+    String custName = "김만기";
+
+    Passenger passenger = new Passenger(custID, custName);
+    passengerDao.insert(passenger);
+    assertEquals(custName, passengerDao.getById(custID).getName());
   }
 }
