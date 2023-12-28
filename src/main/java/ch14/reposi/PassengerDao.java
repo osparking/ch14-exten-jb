4552c49153f21133a35e5a6a7f0cc59a104db1f5
osparking <jbpark03@gmail.com>
2023-12-28 21:54:58 +0900

    인터페이스 메소드 예외 투척 선언

--- src/main/java/ch14/reposi/PassengerDao.java
+++ src/main/java/ch14/reposi/PassengerDao.java
@@ -3,7 +3,7 @@ package ch14.reposi;
 import ch14.passen.Passenger;
 
 public interface PassengerDao {
-  public void insert(Passenger passenger);
+  public void insert(Passenger passenger) throws PassengerExistsException;
 
   public void update(String id, String name);
 
